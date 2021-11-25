6014904113aadddf7615e606653e82ecb8217506
Adam <adamwalkerlondon@gmail.com>
2021-11-25 18:53:45 +0000

    Styled navbar.

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,31 +4,38 @@ import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
 import IconButton from '@mui/material/IconButton';
 import Box from '@mui/material/Box';
+import Paper from '@mui/material/Paper';
 import Typography from '@mui/material/Typography';
 import InputBase from '@mui/material/InputBase';
 import SearchIcon from '@mui/icons-material/Search';
 import Switch from '@mui/material/Switch';
-import { withStyles } from '@mui/material/InputBase';
-import { ClassNames } from "@emotion/react";
+import '../styles/NavbarStyles.css';
 
 
 class Navbar extends Component {
     render() {
         return (
             <div >
-<AppBar position="static" color="primary">
-    <Toolbar>
-        <IconButton  color="inherit">
-        <span>French flag </span> 
-        </IconButton>
-        <Typography className={class.title} variant="h6" color="inherit">
-           App Title
-        </Typography>
-        <Switch />
-        <div className={classes.grow} />
-        <div className={classes.search} />
-    </Toolbar>
-</AppBar>
+                <AppBar position="static" color="primary">
+                    <Toolbar>
+                        <IconButton color="inherit" >
+                            <span>France </span>
+                        </IconButton>
+                        <Typography variant="h6" color="inherit">
+                            App Title
+                        </Typography>
+                        <Switch />
+                        <Paper className="container" >
+                            <InputBase
+                                className="search"
+                                placeholder=" Search"
+                            />
+                            <IconButton type="submit" aria-label="search">
+                                <SearchIcon className="searchIcon" />
+                            </IconButton>
+                        </Paper>
+                    </Toolbar>
+                </AppBar>
             </div>
         )
     }
